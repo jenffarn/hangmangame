c7e7acd8eb2573aab4107ee82b5540675dc5a69e
jenffarn <jenfarn@hotmail.com>
2020-09-24 20:25:36 +0800

    added comments

--- hangmangame.py
+++ hangmangame.py
@@ -1,3 +1,4 @@
+# open text file containg the words and add them to an array
 hangman_words = []
 
 hangman_text = open("hangmanwordlist.txt")
@@ -7,8 +8,7 @@ for word in hangman_text:
 
 hangman_text.close()
 
-import random
-
+# make a function to return a string containing secret and guessed letters
 def update_word(secret, show, guess):
     new = ""
     for i in range(len(secret)):
@@ -21,25 +21,33 @@ def update_word(secret, show, guess):
             new = new + show[i]
     return new
 
+# chose a random secret word from array
+import random
+
 secret_word = random.choice(hangman_words)
 updated = ""
 count = 0
 
+# display secret word as string of "-"s
 for i in range(len(secret_word)):
     updated = updated + "-"
 
 print(updated)
 
+# initialise array containing wrong guesses
 wrong_list = []
 
+# main program
 while True:
     guess_letter = input("Guess a letter: ")
     guess_letter = guess_letter.lower()
 
+    # catch cases of multiple letters or non letters
     if len(guess_letter) > 1 or guess_letter.isalpha() == False:
         print("Not a valid guess")
         continue
     
+    # player guessed a wrong letter again
     if not guess_letter in secret_word:       
         if guess_letter in wrong_list:
             print("You have already guessed this letter")
@@ -55,6 +63,6 @@ while True:
         print("You won!")
         break
     elif count >= 10:
-        print("you lost!")
+        print("you lost!") # player exceeded 10 wrong guesses
         print("The secret word is " + secret_word)
         break
